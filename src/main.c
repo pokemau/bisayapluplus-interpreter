a867ed270ce317e07928502b94ccf9a090014fb3
pokemau <rentillosa90@gmail.com>
2025-02-13 22:18:00 +0800

    get file path

--- src/main.c
+++ src/main.c
@@ -1,6 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define INCREASE 1024
+
+#include "scanner.h"
+
+struct Scanner s;
+
+void error(int line, const char *msg) {
+    fprintf(stderr, "Line: [%d], ERROR: %s\n", line, msg);
+    exit(1);
+}
+
+void run_file(const char *file_name) {
+
+    FILE *f;
+    f = fopen(file_name, "r");
+    if (f == NULL) {
+        fprintf(stderr, "ERROR: File not found!\n");
+        exit(1);
+    }
+
+    // https://www.youtube.com/watch?v=Oa_ji3dDFew
+    size_t allocated = INCREASE;
+    size_t total = 0;
+
+    char *data = malloc(allocated);
+
+    while (!feof(f) && !ferror(f)) {
+        data[total] = fgetc(f);
+        total++;
+        if (total >= allocated) {
+            allocated += INCREASE;
+            data = realloc(data, allocated);
+        }
+    }
+
+    data = realloc(data, total);
+    data[total-1] = '\0';
+
+    // scanner stuff here
+    s = scanner_create(&(scanner_source) {
+        .len = total,
+        .data = data
+    });
+
+    scanner_token(&s);
+
+
+    fclose(f);
+    free(data);
+}
 
 int main(int argc, char *argv[]) {
 
-    printf("hello rold\n");
+    if (argc == 2) {
+        run_file(argv[1]);
+    } else {
+        fprintf(stderr, "ERROR: Invalid arguments!\n");
+        return 1;
+    }
+
+    return 0;
 }
